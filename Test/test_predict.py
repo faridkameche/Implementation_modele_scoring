495a5f81636266190a528a06d42a0bdcba076d71
Farid Kameche <farid.kameche@outlook.com>
2023-05-26 16:36:00 +0200

    Mise à jour test_predict

--- Test/test_predict.py
+++ Test/test_predict.py
@@ -11,6 +11,24 @@ import pytest
 
 pipeline_lgbm = joblib.load("Outputs/pipeline_lgbm.joblib")
 
+# def predict():
+
+#     Prop_voiture = np.log10(np.random.randint(0, 2)+1)
+#     Prop_maison = np.log10(np.random.randint(0, 2)+1)
+#     Reg_pop_rel = np.log10((np.random.randint(0, 1e6)/1e6)+1)
+#     Pret_retard = np.log10(np.random.randint(0, 101)+1)
+#     Pret_annul = np.log10(np.random.randint(0, 33)+1)
+#     Pret_renouvelable = np.log10(np.random.randint(0, 33)+1)
+#     Business = np.log10(np.random.randint(0, 2)+1)
+#     Married = np.log10(np.random.randint(0, 2)+1)
+
+#     valeur_form = [Prop_voiture, Prop_maison, Reg_pop_rel, Pret_retard, Pret_annul, Pret_renouvelable, Business, Married]
+#     val = [np.array(valeur_form)]
+
+#     prediction_p = pipeline_lgbm.predict_proba(val)[0][1]
+
+#     return prediction=round(100*prediction_p,2)
+
 def test_predict():
     liste_feat = []
     for i in range (100):
@@ -36,13 +54,13 @@ def test_predict():
     assert (np.unique(diff_proba)) == 1
 
     
-def predict_str():
-    x_str = ["test", "string", "ne", "fonctionne", "pas", "avec", "le", "modèle"]
-    return pipeline_lgbm.predict_proba(x_str)
+# def predict_str():
+#     x_str = ["test", "string", "ne", "fonctionne", "pas", "avec", "le", "modèle"]
+#     return pipeline_lgbm.predict_proba(x_str)
 
-def test_predict_str():
-    with pytest.raises(ValueError):
-        predict_str()
+# def test_predict_str():
+#     with pytest.raises(ValueError):
+#         predict_str()
 
 
 # In[ ]:
